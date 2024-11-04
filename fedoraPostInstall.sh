dca1b55081615b5b162abe08caa440907e55ec60
Rafael Nolasco <133177948+RflNolasco@users.noreply.github.com>
2024-11-04 20:29:01 -0300

    Adicionado alguns softwares básicos
    Adicionado:
    - dnf dragora
    - gnome software
    - vlc

--- fedoraPostInstall.sh
+++ fedoraPostInstall.sh
@@ -30,3 +30,10 @@ sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
 sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
 sudo dnf groupupdate sound-and-video -y
 sudo dnf install amrnb amrwb faad2 flac gpac-libs lame libde265 libfc14audiodecoder mencoder x264 x265 ffmpegthumbnailer -y
+
+# Lojas de apps (RPM Fusion e Flatpak)
+sudo dnf install dnfdragora
+sudo dnf install gnome-software 
+
+# Player de midia
+sudo dnf install vlc
